de0fc1993ac98a0a78b5cc3387253def0e1489d5
Pradeep Joshi <pradeepjoshi@Pradeeps-MacBook-Pro.local>
2024-04-22 10:39:11 +0530

    middlewhere removed

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,25 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
-import { getSession } from "next-auth/react";
+// import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+// import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
+// import { getSession } from "next-auth/react";
 
-export const config = {
-  matcher: ["/admin/:path*", "/node/:path*", "/parcel/:path*", "/admin/:path*"],
-};
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const requestForNextAuth = {
-    headers: {
-      cookie: req.headers.get("cookie"),
-    },
-  };
+// export const config = {
+//   matcher: ["/admin/:path*", "/node/:path*", "/parcel/:path*", "/admin/:path*"],
+// };
+// export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+//   const requestForNextAuth = {
+//     headers: {
+//       cookie: req.headers.get("cookie"),
+//     },
+//   };
 
-  const session = await getSession({ req: requestForNextAuth });
+//   const session = await getSession({ req: requestForNextAuth });
 
-  if (session) {
-    return NextResponse.next();
-  } else {
-    const signInPage = "/signin";
-    const signInUrl = new URL(signInPage, req.nextUrl.origin);
-    signInUrl.searchParams.append("callbackUrl", req.url);
-    return NextResponse.redirect(signInUrl);
-  }
-}
+//   if (session) {
+//     return NextResponse.next();
+//   } else {
+//     const signInPage = "/signin";
+//     const signInUrl = new URL(signInPage, req.nextUrl.origin);
+//     signInUrl.searchParams.append("callbackUrl", req.url);
+//     return NextResponse.redirect(signInUrl);
+//   }
+// }
